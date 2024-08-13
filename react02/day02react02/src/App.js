03c515363bc66b5ff6b9ba968fc8f4e8ee50b355
Hoto-Mocha <mtj2010@naver.com>
2024-08-13 21:27:42 +0900

    day14 과제 업로드

--- react02/day02react02/src/App.js
+++ react02/day02react02/src/App.js
@@ -1,16 +1,35 @@
+import React, { useState } from 'react';
 import "./App.css";
 import Input from "./components/Input";
 import Output from "./components/Output";
 
 const App = () => {
+    // useState() 훅을 이용해서 state 생성
+    // Input 컴포넌트에서 데이터를 추가하고
+    // Output 컴포넌트에서 데이터를 접근할 수 있다.
+    const [todoListArr, setTodoList] = React.useState([
+        {no:101, title:"운동하기1", done:false},
+        {no:102, title:"운동하기2", done:false},
+        {no:103, title:"운동하기3", done:false}
+    ]);
+    const [noCnt, setNoCnt] = useState(104);
+    
+    function appendItem(title) {
+        // setTodoList 함수를 이용해서 데이터 갱신
+        // 스프레드 연산자를 사용하면 편리하다.
+        setNoCnt(noCnt + 1);
+        const newItem = {no:noCnt, title:title, done:false};
+        setTodoList([...todoListArr, newItem]);
+    }
+
     return (<div>
         <header className="jumbotron">
             <h1>Todo List</h1>
         </header>
         {/* 입력 기능 */}
-        <Input />
-        {/* 목록 출력 기능 */}
-        <Output />
+        <Input appendItem = {appendItem}/>
+        {/* 목록 출력 기능 - 추가된 속성은 props로 전달 */}
+        <Output todoListArr = {todoListArr} setTodoList = {setTodoList} />
     </div>);
 }
 
--- react02/day02react02/src/components/Output.js
+++ react02/day02react02/src/components/Output.js
@@ -1,6 +1,9 @@
-export default () => {
+export default (probs) => {
+    let todoListArr = probs.todoListArr;
+    let setTodoList = probs.setTodoList;
+    // console.dir(probs.todoListArr);
     return (<div>
-        <table id="todoListTbl" class="table table-hover">
+        <table id="todoListTbl" className="table table-hover">
             <thead>
                 <tr>
                     <th>Confirm</th>
@@ -9,15 +12,29 @@ export default () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>
-                        <input type="checkbox"></input>
-                    </td>
-                    <td>미니프로젝트 만들기</td>
-                    <td>
-                        <button class="btn btn-danger" type="button">삭제</button>
-                    </td>
-                </tr>
+                {todoListArr.map((item, idx)=>{
+                    // 중괄호가 여러 줄에 걸쳐 있으면 return을 넣어야 한다
+                    return(
+                        <tr key={item.no}>
+                            <td>
+                                <input onChange={() => {
+                                    let tempArr = [...todoListArr];
+                                    tempArr[idx].done = !tempArr[idx].done;
+                                    setTodoList(tempArr);
+                                }} type="checkbox"></input>
+                            </td>
+                            <td style={{textDecoration: item.done ? 'line-through' : ''}}>{item.title}</td>
+                            <td>
+                                <button onClick={() => {
+                                    // console.log(...todoListArr);
+                                    let tempArr = [...todoListArr];
+                                    tempArr.splice(idx, 1);
+                                    setTodoList(tempArr);
+                                }} className="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                    );
+                })}
             </tbody>
         </table>
     </div>);
