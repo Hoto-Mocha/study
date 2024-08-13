07cb3d83654dd25c3d9a42ec80af509101831aa7
Hoto-Mocha <mtj2010@naver.com>
2024-08-13 13:50:26 +0900

    day14 css

--- react02/day02react02/src/App.js
+++ react02/day02react02/src/App.js
@@ -1,7 +1,36 @@
-function App() {
-  return (<>
-    {/* 내용 */}
-  </>);
+import './App.css';
+
+const App = () => {
+    return (<div>
+        <h1>Hello React World</h1>
+        <button className='btn btn-primary'>클릭</button>
+        <table class="table table-dark table-hover">
+            <thead>
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+            </tr>
+            <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>);
 }
 
 export default App;
