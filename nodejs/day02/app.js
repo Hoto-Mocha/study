9ac1da0f2828f3d75f61723e1d0e4d577ca15b5c
Hoto-Mocha <mtj2010@naver.com>
2024-08-22 11:40:06 +0900

    nodejs day02 - 페이지 및 로그인 기능 일부 구현

--- nodejs/day02/app.js
+++ nodejs/day02/app.js
@@ -1,48 +1,77 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('port', 3000);
-app.set('views', 'views');
-app.set('view engine', 'ejs')
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+// post 방식의 파라미터를 전달 받기 위한 설정
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/home', (req, res) => {
-    req.app.render('Home', {}, (err, html) => {
-        res.end(html)
-    })
-})
+const memberList = [
+    {no:101, id:'user01', password:'1234', name:'홍길동', email:'hong@example.com'},
+    {no:102, id:'user02', password:'1234', name:'홍길동', email:'hong@example.com'},
+    {no:103, id:'user03', password:'1234', name:'홍길동', email:'hong@example.com'},
+    {no:104, id:'user04', password:'1234', name:'홍길동', email:'hong@example.com'}
+];
+let noCnt = 105;
 
+app.get("/home", (req, res) => {
+    req.app.render("home/Home", {}, (err, html)=>{
+        res.end(html);
+    });
+});
 
-app.get('/profile', (req, res) => {
-    req.app.render('Profile', {}, (err, html) => {
-        res.end(html)
-    })
-})
+app.get("/profile", (req, res) => {
+    req.app.render("profile/Profile", {}, (err, html)=>{
+        res.end(html);
+    });
+});
 
+app.get("/member", (req, res) => {
+    req.app.render("member/Member", {}, (err, html)=>{
+        res.end(html);
+    });
+});
 
-app.get('/member', (req, res) => {
-    req.app.render('Member', {}, (err, html) => {
-        res.end(html)
-    })
-})
+app.get("/login", (req, res) => {
+    req.app.render("member/Login", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+app.post("/login", (req, res) => {
+    console.log(req.body.id, req.body.password);
+    res.redirect('/member');
+});
 
+app.get("/joinus", (req, res) => {
+    // 회원 가입 ejs 페이지 foward
+    req.app.render("member/Joinus", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+app.post("/joinus", (req, res) => {
+    // 회원 가입 처리 후 목록으로 리다이렉트
+    res.redirect('/member');
+});
 
-app.get('/gallery', (req, res) => {
-    req.app.render('Gallery', {}, (err, html) => {
-        res.end(html)
-    })
-})
+app.get("/gallery", (req, res) => {
+    req.app.render("gallery/Gallery", {}, (err, html)=>{
+        res.end(html);
+    });
+});
 
-
-app.get('/shop', (req, res) => {
-    req.app.render('Shop', {}, (err, html) => {
-        res.end(html)
-    })
-})
+app.get("/shop", (req, res) => {
+    req.app.render("shop/Shop", {}, (err, html)=>{
+        res.end(html);
+    });
+});
 
 const server = http.createServer(app);
-server.listen(app.get('port'), () => {
-    console.log(`서버 실행 중 >>> http://localhost:${app.get('port')}`);
+server.listen(app.get('port'), ()=>{
+    console.log(`Run on server >>> http://localhost:${app.get('port')}`);
 });
